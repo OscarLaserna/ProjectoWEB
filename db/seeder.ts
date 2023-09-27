cfeddb5c2d3f99b928fef2199f254371671f26d2
ÓSCAR LASERNA BACHILLER <Oscar.Laserna@alu.uclm.es>
2023-09-27 19:32:19 +0200

    base de datos

--- db/seeder.ts
+++ db/seeder.ts
@@ -8,20 +8,52 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const products: Product[] = [
   {
-    name: 'Earthen Bottle',
-    price: 39.95,
-    img: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    name: 'Puma rbd',
+    price: 55.95,
+    img: 'https://img01.ztat.net/article/spp-media-p1/9efea25f72194651933bfe207b28909c/0558f6ed2596417d8bfb0073491da2b1.jpg',
     brand: 'puma',
-    colour: 'green',
+    colour: 'White',
     description: 'What a shoe!',
   },
   {
-    name: 'Nomad Tumbler',
-    price: 39.95,
-    img: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    name: 'Puma infusion',
+    price: 49.95,
+    img: 'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1200,h_1200/global/377893/01/sv01/fnd/EEA/fmt/png',
     brand: 'puma',
-    colour: 'green',
-    description: 'Yet another item',
+    colour: 'Black',
+    description: 'Good shoes for running',
+  },
+  {
+    name: 'ZAPATILLA 3MC VULC',
+    price: 74.95,
+    img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/30763fe1599d438ab4eba89100352700_9366/Zapatilla_3MC_Vulc_Blanco_B22705_01_standard.jpg',
+    brand: 'adidas',
+    colour: 'Black',
+    description: 'Amazing shoes',
+  },
+  {
+    name: 'ZAPATILLA FORUM LOW',
+    price: 50.95,
+    img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/09c5ea6df1bd4be6baaaac5e003e7047_9366/Zapatilla_Forum_Low_Blanco_FY7756_01_standard.jpg',
+    brand: 'adidas',
+    colour: 'Blue and White',
+    description: 'To much style',
+  },
+  {
+    name: 'Nike Dunk Low',
+    price: 100.95,
+    img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/22beef05-be64-43cd-92a9-6406114a261d/dunk-low-zapatillas-Z1v6wk.png',
+    brand: 'nike',
+    colour: 'Green',
+    description: 'Casual shoes',
+  },
+  {
+    name: 'NIKE AIR JORDAN 1 RETRO LOW BRED TOE',
+    price: 80.95,
+    img: 'https://it.basketzone.net/zdjecia/2022/05/03/705/51/NIKE_AIR_JORDAN_1_RETRO_LOW_BLACK_TOEs-mini.jpg?mini',
+    brand: 'nike',
+    colour: 'Black and Red',
+    description: 'Good shoes for basket',
   },
 ];
 
@@ -37,7 +69,7 @@ async function seed() {
   };
   const conn = await mongoose.connect(MONGODB_URI, opts);
 
-  await conn.connection.db.dropDatabase();
+  //await conn.connection.db.dropDatabase();
 
   const insertedProducts = await Products.insertMany(products);
   const user: User = {
@@ -56,9 +88,26 @@ async function seed() {
         product: insertedProducts[1]._id,
         qty: 5,
       },
+      {
+        product: insertedProducts[2]._id,
+        qty: 1,
+      },
+      {
+        product: insertedProducts[3]._id,
+        qty: 6,
+      },
+      {
+        product: insertedProducts[4]._id,
+        qty: 3,
+      },
+      {
+        product: insertedProducts[5]._id,
+        qty: 10,
+      },
     ],
     orders: [],
   };
+  //const res = await Users.create(user);
   const retrievedUser = await Users
   .findOne({ email: 'johndoe@example.com' })
   .populate('cartItems.product');
