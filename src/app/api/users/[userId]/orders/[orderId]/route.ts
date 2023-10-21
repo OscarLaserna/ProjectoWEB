2ab6199fbf35b68435fd234d3f4373a48838fa77
ÓSCAR LASERNA BACHILLER <Oscar.Laserna@alu.uclm.es>
2023-10-21 15:07:44 +0200

    Get orderid autorizacion

--- src/app/api/users/[userId]/orders/[orderId]/route.ts
+++ src/app/api/users/[userId]/orders/[orderId]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Types } from 'mongoose';
 import { getOrderById, OrderResponse } from '@/lib/handlers';
+import { Session } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/authOptions';
 
 
 export async function GET(
@@ -11,9 +14,19 @@ export async function GET(
         params: {userId:string, orderId:string};
     }
 ): Promise<NextResponse<OrderResponse> | {}>{
+    const session: Session | null = await getServerSession(authOptions);
+    if (!session?.user) {
+        return NextResponse.json({}, { status: 401 });
+    }
+
     if (!Types.ObjectId.isValid(params.userId)||!Types.ObjectId.isValid(params.orderId)) {
         return NextResponse.json({}, { status: 400 });
     }
+
+    if (session.user._id !== params.userId) {
+        return NextResponse.json({}, { status: 403 });
+    }
+    
     const order = await getOrderById(params.userId,params.orderId);
     if(order===null){
         return NextResponse.json({},{status:404});
